db8690e1b762b3e604fdf09ea35fafdea963d2a9
Shakti Singh <shaktipratap10@gmail.com>
2015-06-29 23:43:25 +0530

    associated balance factor with each node

--- tree.c
+++ tree.c
@@ -2,7 +2,7 @@
 typedef struct tree tree;
 struct tree
 {
-    int value,height;
+    int value,height,balancef;
     tree *left,*right,*parent;
 };
 tree *root=NULL;
@@ -38,10 +38,6 @@ int main()
             if(error==-1)
             {
                 printf("\t   Sorry the element does not exists.");
-            }
-            else if(error=1)
-            {
-                printf("\t   Yes,%d is present in the tree.",value);
             }
             break;
         case 0:
@@ -121,7 +117,28 @@ void insert(int value)
     }
     if(copy->parent!=NULL)
     {
+        copy->balancef=0;
         copy=copy->parent;
+    }
+    copy->balancef=balance();
+}
+int balance()
+{
+    if(copy->right==NULL&&copy->left==NULL)
+    {
+        return 0;
+    }
+    else if(copy->right==NULL&&copy->left!=NULL)
+    {
+        return copy->left->height;
+    }
+    else if(copy->left==NULL&&copy->right!=NULL)
+    {
+        return -(copy->right->height);
+    }
+    else
+    {
+        return ((copy->left->height)-(copy->right->height));
     }
 }
 void updateHeight()
@@ -138,7 +155,181 @@ int search(int value)
     {
         if(value==copy->value)
         {
-            printf("at height %d is present element:",copy->height);
+            printf("yes,%d is present at height %d with balance factor of %d:",value,copy->height,copy->balancef);
+
+            return 1;
+        }
+        else if(value<copy->value)
+        {
+            copy=copy->left;
+            search(value);
+        }
+        else if(value>copy->value)
+        {
+            copy=copy->right;
+            search(value);
+        }
+    }
+}
+#include<stdio.h>
+typedef struct tree tree;
+struct tree
+{
+    int value,height,balancef;
+    tree *left,*right,*parent;
+};
+tree *root=NULL;
+tree *copy;
+int p=0;
+int main()
+{
+    int n,flag=1,value,error=-1;
+    while(flag)
+    {
+        printf("\n\n --------------------------------------------------------------------");
+        printf("\n\n|-------------------->1.Insert in a tree.                            |");
+        printf("\n\n|-------------------->2.search in a tree.                            |");
+        printf("\n\n|-------------------->0.to exit                                      |");;
+        printf("\n\n --------------------------------------------------------------------\n\n\t");
+        printf("\n\t\tEnter your choice:");
+        scanf("%d",&n);
+        switch(n)
+        {
+        case 1:
+            printf("\t   Enter the value o be inserted.");
+            scanf("%d",&value);
+            insert(value);
+            p=0;
+            copy=root;
+            printf("\t\tInsertion Successful!");
+            break;
+        case 2:
+            printf("\t   enter the value to be searched.");
+            scanf("%d",&value);
+            copy=root;
+            error=search(value);
+            if(error==-1)
+            {
+                printf("\t   Sorry the element does not exists.");
+            }
+            break;
+        case 0:
+            flag=0;
+            break;
+        default :
+            printf("\n\t\tInvalid entry!retry");
+        }
+    }
+    return 0;
+}
+void insert(int value)
+{
+    if(root==NULL)
+    {
+        root=(tree *)malloc(sizeof(tree));
+        root->value=value;
+        root->left=root->right=NULL;
+        root->parent=NULL;
+        root->height=1;
+        copy=root;
+    }
+    else
+    {
+        if(value==copy->value)
+        {
+            printf("\tThe value you are trying to insert is already present.");
+            exit(0);
+        }
+        if(value<copy->value)
+        {
+
+            if(copy->left==NULL)
+            {
+                copy->left=(tree *)malloc(sizeof(tree));
+                copy->left->value=value;
+                copy->left->left=copy->left->right=NULL;
+                copy->left->parent=copy;
+                if(copy->right==NULL)
+                {
+                    p=1;
+                }
+                copy=copy->left;
+                copy->height=1;
+            }
+            else
+            {
+                copy=copy->left;
+                insert(value);
+            }
+        }
+        else if(value>copy->value)
+        {
+            if(copy->right==NULL)
+            {
+                copy->right=(tree *)malloc(sizeof(tree));
+                copy->right->value=value;
+                copy->right->left=copy->right->right=NULL;
+                copy->right->parent=copy;
+                if(copy->left==NULL)
+                {
+                    p=1;
+                }
+                copy=copy->right;
+                copy->height=1;
+            }
+            else
+            {
+                copy=copy->right;
+                insert(value);
+            }
+        }
+    }
+    if(p==1)
+    {
+        updateHeight();
+    }
+    if(copy->parent!=NULL)
+    {
+        copy->balancef=0;
+        copy=copy->parent;
+    }
+    copy->balancef=balance();
+}
+int balance()
+{
+    if(copy->right==NULL&&copy->left==NULL)
+    {
+        return 0;
+    }
+    else if(copy->right==NULL&&copy->left!=NULL)
+    {
+        return copy->left->height;
+    }
+    else if(copy->left==NULL&&copy->right!=NULL)
+    {
+        return -(copy->right->height);
+    }
+    else
+    {
+        return ((copy->left->height)-(copy->right->height));
+    }
+}
+void updateHeight()
+{
+    copy->parent->height=copy->height+1;
+}
+int search(int value)
+{
+    if(copy==NULL)
+    {
+        return -1;
+    }
+    else
+    {
+        if(value==copy->value)
+        {
+            printf("yes,%d is present at height %d with balance factor of %d:",value,copy->height,copy->balancef);
+
             return 1;
         }
         else if(value<copy->value)
